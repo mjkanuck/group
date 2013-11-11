87d6333a30a8f15eb507c91cdc3ea80df207cd64
Matt <mjkanuck@gmail.com>
2013-11-11 10:37:59 -0500

    initial commit

---
+++ groupwork/work.cpp
@@ -0,0 +1,33 @@
+/*
+ * work.cpp
+ *
+ *  Created on: Nov 11, 2013
+ *      Author: mkyan
+ */
+
+
+
+
+/*
+ * main.cpp
+ *
+ *  Created on: Nov 5, 2013
+ *      Author: mkyan
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+//
+
+int main(int argc, char *argv[]) {
+
+	cout << "hello groupwork";
+	cout << "\ndone..\n";
+
+
+
+	return(0);
+}
